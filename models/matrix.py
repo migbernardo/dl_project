eaaa82ae3c6ec869b2956db4fc08cb2e7fd4bf2b
migbernardo <mbdexb@gmail.com>
2022-04-06 12:06:36 +0100

    new models

--- models/matrix.py
+++ models/matrix.py
@@ -1,6 +1,8 @@
 import os
+import numpy as np
 from keras.models import load_model
 from mb_initial_model import read_data
+from sklearn.metrics import multilabel_confusion_matrix
 
 if __name__ == '__main__':
 
@@ -14,6 +16,13 @@ if __name__ == '__main__':
     os.chdir(os.path.abspath(os.pardir))
     os.chdir(os.path.join(os.path.abspath(os.curdir), 'models'))
 
-    model = load_model('baseline_shallow_cnn.h5')
-    results = model.evaluate(X_test, y_test)
+    model = load_model('1_shallow_cnn.h5')
+    pred = model.predict(X_test)
 
+    preds = []
+    for img in pred:
+        encode = np.zeros(10)
+        encode[int(np.argmax(img))] = 1
+        preds.append(encode.tolist())
+
+    conf = multilabel_confusion_matrix(y_test, preds)
