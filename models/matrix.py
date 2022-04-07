e0d844c60527f5dacaec843be4e67fe5e18d851b
migbernardo <mbdexb@gmail.com>
2022-04-07 11:49:07 +0100

    more models

--- models/matrix.py
+++ models/matrix.py
@@ -1,10 +1,15 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from keras.models import load_model
 from mb_initial_model import read_data
-from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import confusion_matrix
 
 if __name__ == '__main__':
+
+    labels = ['AnnualCrop', 'Forest', 'HerbaceousVegetation', 'Highway', 'Industrial', 'Pasture', 'PermanentCrop',
+              'Residential', 'River', 'SeaLake']
 
     os.chdir(os.path.abspath(os.pardir))
     os.chdir(os.path.join(os.path.abspath(os.curdir), 'data', 'processed'))
@@ -19,10 +24,15 @@ if __name__ == '__main__':
     model = load_model('1_shallow_cnn.h5')
     pred = model.predict(X_test)
 
-    preds = []
-    for img in pred:
-        encode = np.zeros(10)
-        encode[int(np.argmax(img))] = 1
-        preds.append(encode.tolist())
-
-    conf = multilabel_confusion_matrix(y_test, preds)
+    ax = plt.subplot()
+    cm = confusion_matrix(np.asarray(y_test).argmax(axis=1), np.asarray(pred).argmax(axis=1), normalize='true')
+    cm = np.round(cm, decimals=2)
+    sns.heatmap(cm, annot=True, fmt='g', ax=ax)
+    ax.set_xlabel('Predicted labels')
+    ax.set_ylabel('True labels')
+    ax.set_title('Confusion Matrix')
+    ax.xaxis.set_ticklabels(labels)
+    ax.yaxis.set_ticklabels(labels)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=30)
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=30)
+    plt.show()
